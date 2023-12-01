8f590123da6ef396747696b1170afe4743a95415
Jethro Ordaneza <jet.oneza@gmail.com>
2023-12-01 08:26:34 +0800

    feat: create initial template for rust

---
+++ templates/rust/template.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = part_1(input);
+}
+
+fn part_1(input: &str) -> String {
+    "sample".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = part_1("");
+        assert_eq!(result, "sample".to_string());
+    }
+}
