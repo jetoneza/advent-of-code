dcb3b8aaf04844af5c302bd958e9013481aa1c67
Jethro Ordaneza <jet.oneza@gmail.com>
2023-12-02 14:09:15 +0800

    solution: day-2 part 1

---
+++ 2023/rust/day-02/src/main.rs
@@ -0,0 +1,11 @@
+use::day_02::part_1;
+
+// Advent of Code - Day 2 Solution
+// Link: https://adventofcode.com/2023/day/2
+fn main() {
+    let input = include_str!("./input.txt");
+
+    let part_1_answer = part_1::execute(input);
+
+    println!("Answer for part 1: {}", part_1_answer);
+}
